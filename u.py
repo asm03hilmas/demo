a3419bc4462ec8d564a6575410d0d7cc30af71f4
asm03hilmas <119746982+asm03hilmas@users.noreply.github.com>
2022-12-03 07:18:07 -0800

    Add files via upload

---
+++ u.py
@@ -0,0 +1,15 @@
+
+
+
+def add (self):
+    self.name=input('hilmas:   ')
+    self.name1=input('mohamed:')
+    
+    print()
+def sub (self):
+         self.name=input('hilmas:   ')
+         self.name1=input('mohamed:')
+ 
+
+a= add()
+a.subs
